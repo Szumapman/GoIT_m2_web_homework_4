bf711527f7e8a14d9ed9dd83d8499ddc81ad6d55
Paweł Szumański <pawel.szumanski@tuta.io>
2024-01-26 20:25:41 +0100

    Update main.py

--- goit_m2_web_homework_4/main.py
+++ goit_m2_web_homework_4/main.py
@@ -10,6 +10,9 @@ import threading
 import logging
 
 
+APP_PATH = Path(__file__).parent.absolute()
+
+
 class HttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         data = self.rfile.read(int(self.headers["Content-Length"]))
@@ -19,7 +22,7 @@ class HttpHandler(BaseHTTPRequestHandler):
             for key, value in [item.split("=") for item in data_parse.split("&")]
         }
         byte_data = json.dumps(data_dict).encode()
-        run_socket_client("127.0.0.1", 5000, byte_data)
+        run_socket_client("0.0.0.0", 5000, byte_data)
         self.send_response(302)
         self.send_header("Location", "templates/index.html")
         self.end_headers()
@@ -27,14 +30,17 @@ class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parse_result = urllib.parse.urlparse(self.path)
         if parse_result.path == "/":
-            self.send_html_file("./templates/index.html")
+            self.send_html_file(APP_PATH.joinpath("templates/index.html"))
         elif parse_result.path == "/message.html":
-            self.send_html_file("./templates/message.html")
+            self.send_html_file(APP_PATH.joinpath("templates/message.html"))
         else:
-            if Path().joinpath(parse_result.path[1:]).exists():
+            if APP_PATH.joinpath(Path().joinpath(parse_result.path[1:])).exists():
                 self.send_static()
             else:
-                self.send_html_file("templates/error.html", 404)
+                self.send_html_file(
+                    APP_PATH.joinpath("/templates/error.html"),
+                    404,
+                )
 
     def send_html_file(self, filename, status_code=200):
         self.send_response(status_code)
@@ -51,7 +57,8 @@ class HttpHandler(BaseHTTPRequestHandler):
         else:
             self.send_header("Content-Type", "text/plain")
         self.end_headers()
-        with open(f".{self.path}", "rb") as file:
+        static_path = str(APP_PATH) + self.path
+        with open(static_path, "rb") as file:
             self.wfile.write(file.read())
 
 
@@ -72,12 +79,12 @@ def run_socket_server(ip, port, data_dict, data_path):
     except KeyboardInterrupt:
         logging.debug(f"Destroy socket server")
     finally:
-        logging.debug(f"finally Destroy socket server")
         sock.close()
 
 
 def save_json_data(data_dict, data, data_path):
     data_dict[datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")] = json.loads(data)
+    logging.debug(f"data_dict: {data_dict}")
     with open(data_path, "w") as f:
         json.dump(data_dict, f)
 
@@ -91,7 +98,7 @@ def run_socket_client(host, port, data):
 
 
 def run_http_server(
-    server_class=HTTPServer, handler_class=HttpHandler, ip="127.0.0.1", port=3000
+    server_class=HTTPServer, handler_class=HttpHandler, ip="0.0.0.0", port=3000
 ):
     server_address = (ip, port)
     http_server = server_class(server_address, handler_class)
@@ -116,9 +123,11 @@ def main():
     else:
         with open(data_path, "r") as f:
             data_dict = json.load(f)
-    http_server = threading.Thread(target=run_http_server)
+    http_server = threading.Thread(target=run_http_server, daemon=True)
     socket_server = threading.Thread(
-        target=run_socket_server, args=("127.0.0.1", 5000, data_dict, data_path)
+        target=run_socket_server,
+        args=("0.0.0.0", 5000, data_dict, data_path),
+        daemon=True,
     )
     socket_server.start()
     http_server.start()
